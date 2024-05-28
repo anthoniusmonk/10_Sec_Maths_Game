d108f5cef246b081dfa2b45df84a80055b3b298c
Anthony Gallardo <anthoniusmonk@gmail.com>
2024-05-28 09:20:27 -0600

    timer countdown added

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
   var currentQuestion;
+  var timeLeft = 10;
 
   var randomNumberGenerator = function (size) {
     return Math.ceil(Math.random() * size);
@@ -25,12 +26,25 @@ $(document).ready(function(){
     if(userInput === answer) {
       renderNewQuestion();
       $('#user-input').val('');
+      updateTimeLeft(+1);
     }
   }
 
   $('#user-input').on('keyup', function () {
     checkAnswer(Number($(this).val()), currentQuestion.answer);
   });
+
+  var interval = setInterval(function () {
+    updateTimeLeft(-1)
+    if (timeLeft === 0) {
+      clearInterval(interval);
+    }
+  }, 1000);
+
+  var updateTimeLeft = function (amount) {
+    timeLeft += amount;
+    $('#time-left').text(timeLeft);
+  }
 
   renderNewQuestion();
 });
